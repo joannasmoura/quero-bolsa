3ce07e59eb97d654842d926e1a1d9f1d1b3903fb
Joanna Moura <joannasmoura@gmail.com>
2019-09-12 23:30:05 -0300

    Organização da estrutura inicial do projeto com adição de arquivos .scss

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../../assets/images/logo-querobolsa.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />             
+      </header>
+    </div>
+  );
+}
+
+export default App;
